d15772e588cba1dce741eb7a7a8bebf33a4ff8dc
Quispe Agustin <aguuss.one@gmail.com>
2025-10-24 13:28:27 -0300

    Create ejercicio1.pas

---
+++ Modulo imperativo /PRACTICA 0/ejercicio1.pas
@@ -0,0 +1,56 @@
+program NumAleatorio;
+
+procedure ImprimirNumerosAleatorios ();
+var ale,i: integer;
+begin
+ randomize;                                             
+ for i:=1 to 20 do
+ begin
+	ale := random (100);
+	writeln ('El numero aleatorio generado es: ', ale);
+  end;
+end;
+
+procedure ImprimirNumerosN(N,A,B:integer);
+var ale,i:integer;
+begin
+    randomize;
+    for i:=1 to N do
+    begin
+         ale:= A + random(B - A + 1);
+         writeln('Numero ', i , ': ', ale);
+    end;
+end;
+procedure ImprimirHastaF(F,A,B:integer);
+var 
+ale,cont:integer;
+begin
+	randomize;
+	ale:=A + random(B - A + 1);
+	cont:=1;
+	while(ale <> F)do
+	begin
+        writeln('Numero ',cont, ': ', ale);
+        ale:=A + random(B - A + 1);
+        cont:= cont + 1;
+    end;
+end;
+var
+N,A,B,F:integer;
+begin
+   ImprimirNumerosAleatorios();
+   writeln('Ingrese N: ');
+   readln(N);
+   writeln('Ingrese A');
+   readln(A);
+   writeln('Ingrese B');
+   readln(B);
+   ImprimirNumerosN(N,A,B);
+   writeln('Ingrese F: ');
+   readln(F);
+   writeln('Ingrese A');
+   readln(A);
+   writeln('Ingrese B');
+   readln(B);
+   ImprimirHastaF(F,A,B)
+end.
