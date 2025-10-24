a413f167e63bd5c4d25db7b3d0f59ec8f66b4995
Quispe Agustin <aguuss.one@gmail.com>
2025-10-24 13:50:03 -0300

    Create ejercicio1.java

---
+++ Modulo objetos /PRACTICA 1/ejercicio1.java
@@ -0,0 +1,22 @@
+package Practica1;
+
+import PaqueteLectura.GeneradorAleatorio;
+
+public class ejercicio1 {
+    public static void main (String[]args){
+        int DF=11;  
+        int [] tabla2 = new int[DF]; // indices de 0 a 10
+        int i;
+        for (i=0;i<DF;i++) 
+            tabla2[i]=2*i;
+        
+        System.out.println("2x" + "5" + "="+ tabla2[5]);
+
+    GeneradorAleatorio.iniciar();
+    int num = GeneradorAleatorio.generarInt(12);
+    while (num != 11){
+        System.out.println("2 x " + num + " = " + tabla2[num]);
+        num = GeneradorAleatorio.generarInt(12);
+    }
+    }
+}
